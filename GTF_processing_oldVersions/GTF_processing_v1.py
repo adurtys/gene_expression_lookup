217a1735f6881f68aa7e176d816565b53a245d72
Sanjana Adurty <adurtys@sciget.pmacs.upenn.edu>
2018-06-29 15:08:03 -0400

    created directory for processing drafts

---
+++ GTF_processing_oldVersions/GTF_processing_v1.py
@@ -0,0 +1,54 @@
+# Date Created: 27 June 2018
+# Date Last Modified: 27 June 2018
+# Execution: 
+# This program proceses GENCODE Comprehensive Gene Annotation GTF File for the start and end locations of protein-coding genes. 
+# The output is a file containing locations of protein-coding genes, and maintains both GeneID (ENSG#) and name. 
+
+#!/usr/bin/env python
+
+# read in the GTF file (tab-separated, header = true)
+inFilename = "gencode.v19.annotation.gtf"
+outFilename = "gene_annotations.txt"
+
+inFile = open(inFilename, 'r')
+# outFile = open(outFilename, 'w')
+
+inFile.readline() # skip header line
+
+for line in inFile:
+	line = line.rstrip('\r\n')
+	columns = line.split() # split line on whitespace (tab), returns list of columns
+
+	geneStart = columns[3] + "\t"
+	geneEnd = columns[4] + "\t"
+	otherInfo = columns[8] + "\n"
+
+	print geneStart, geneEnd, otherInfo
+
+	# outFile.write(geneStart)
+	# outFile.write(geneEnd)
+	# outFile.write(otherInfo)
+
+	# process other info for gene_id and gene_name given that gene_type is protein-coding
+	# otherInfo = otherInfo.split(';')
+
+
+
+# parse file for protein-coding genes
+
+# write new file with four columns (GeneID, Gene Name, Start, End)
+
+# for each line: 
+	# determine whether gene already has start and end specified in new file
+	# check both GeneID and Gene Name
+		# if no:
+			# write new line in new file for gene
+		# if yes:
+			# check start position
+			# if GTF_start < newFile_start, then newFile_start == GTF_start
+			
+			# check end position
+			# if GTF_end > newFile_end, then newFile_end == GTF_end
+
+# outFile.close()
+inFile.close()
---
+++ GTF_processing_oldVersions/GTF_processing_v6.py
@@ -0,0 +1,141 @@
+# Date Created: 27 June 2018
+# Date Last Modified: 28 June 2018
+# Execution: python GTF_processing_v3
+# This program proceses GENCODE Comprehensive Gene Annotation GTF File for the start and end locations of protein-coding genes. 
+# The output is a file containing locations of protein-coding genes, and maintains both GeneID (ENSG#) and name. 
+
+#!/usr/bin/env python
+
+# read in the GTF file (TODO: for now, it is the shortened version)
+inFilename = "firstLines.txt"
+inFile = open(inFilename, 'r')
+
+# create data structures to hold information pertaining to each gene
+genes = []
+geneNames = []
+chromNums = []
+geneTypes = []
+geneStartLocations = []
+geneEndLocations = []
+
+# skip first five lines
+for i in range(5):
+        inFile.readline()
+
+for line in inFile:
+   	line = line.rstrip('\r\n')
+	
+	# split line on tab, return list of columns
+	columns = line.split('\t')
+    
+	chromosome = columns[0]
+	geneStart = int(columns[3])
+	geneEnd = int(columns[4])
+	
+	# process otherInfo column by getting rid of key label and quotes
+	otherInfo = columns[8]
+	otherInfo = otherInfo.split(';')
+
+	geneId = otherInfo[0]
+	geneId = geneId.strip('gene_id ')
+	geneId = geneId.strip('"')
+
+	name = otherInfo[4]
+	name = name.strip('gene_name ')
+	name = name.strip('"')
+
+	geneType = otherInfo[2]
+	geneType = geneType.strip('gene_type ')
+	geneType = geneType.strip('"')
+
+	# add info to their respective data structures
+	genes.append(geneId)
+	geneNames.append(name)
+	chromNums.append(chromosome)
+	geneTypes.append(geneType)
+	geneStartLocations.append(geneStart)
+	geneEndLocations.append(geneEnd)
+
+inFile.close()
+
+numGenes = len(genes)
+print "There are", numGenes, "total genes in this file."
+
+# create new data structures to store information for unique protein-coding genes
+protCodingGenes = []
+uniqGenes = []
+uniqGeneName = {}
+uniqGeneChrom = {}
+uniqGeneStart = {}
+uniqGeneEnd = {}
+
+# parse gene list for protein-coding genes
+for id in range(numGenes):
+	if geneTypes[id] == "protein_coding":
+		protCodingGenes.append(genes[id])
+
+numProtGenes = len(protCodingGenes)
+print "There are", numProtGenes, "total protein-coding genes in this file."
+
+# parse protein-coding gene list for unique genes
+for id in range(numProtGenes):
+	key = protCodingGenes[id]
+
+	if key not in uniqGenes:
+		# create list of unique protein-coding genes
+		uniqGenes.append(key)
+
+		# edit dictionaries containing gene information
+		# use the GeneID stored in the protein-coding gene list as keys
+		uniqGeneName[key] = geneNames[id]
+		uniqGeneChrom[key] = chromNums[id]
+
+		# add start location if not already in the dictionary
+		if key not in uniqGeneStart:
+			uniqGeneStart[key] = geneStartLocations[id]
+		# only modify start location if new position is smaller than current one
+		elif geneStartLocations[id] < uniqGeneStart[key]:
+			uniqGeneStart[key] = geneStartLocations[id]
+
+		# add end location if not already in the dictionary
+		if key not in uniqGeneEnd:
+			uniqGeneEnd[key] = geneEndLocations[id]
+		# only modify end location if new position is larger than current one
+		elif geneEndLocations[id] > uniqGeneEnd[key]:
+			uniqGeneEnd[key] = geneEndLocations[id]
+
+
+numUniqGenes = len(uniqGenes)
+print "There are ", numUniqGenes, "unique protein-coding genes in this file."
+
+for uniqId in uniqGenes:
+	print uniqId, "\t", uniqGeneName[uniqId], "\t", uniqGeneChrom[uniqId]
+
+
+# create a new file for start and end positions of only protein-coding genes
+# outFilename = "gene_annotations_v2.txt"
+# outFile = open(outFilename, 'w')
+
+# tab = "\t"
+# newline = "\n"
+
+# # convert start and end positions back to strings
+# for geneId in geneStartDict:
+# 	geneStartDict[geneId] = str(geneStartDict[geneId])
+# for geneId in geneEndDict:
+# 	geneEndDict[geneId] = str(geneEndDict[geneId])
+
+# string = geneId + tab + geneNameDict[geneId] + tab + geneStartDict[geneId] ...
+
+# print "writing file now"
+# for geneId in uniqGenes:
+# 	 outFile.write(geneId)
+# 	 outFile.write(tab)
+# 	 outFile.write(geneNameDict[geneId])
+# 	 outFile.write(tab)
+# 	 outFile.write(geneStartDict[geneStart])
+# 	 outFile.write(tab)
+# 	 outFile.write(geneEndDict[geneEnd])
+# 	 outFile.write(newline)
+
+# outFile.close()
