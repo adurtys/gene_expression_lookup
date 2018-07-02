2a5dd6f972b24ccf44957b315a2b17a808a1500f
adurtys <adurtys@gmail.com>
2018-07-02 10:15:56 -0400

    edited lookup v1

--- expression_lookup_v1.py
+++ expression_lookup_v1.py
@@ -1,7 +1,35 @@
 # Date Created: 28 June 2018
-# Date Last Modified: 28 June 2018
-# TODO (other parameters?): Execution: python expression_lookup.py
+# Date Last Modified: 29 June 2018
+# TODO (other parameters?): Execution: python expression_lookup_v1.py
 # TODO: This program ...
 
 #!/usr/bin/env python
 
+# read in argument from user for snp (chr#:location)
+# import sys
+# snp = sys.argv[0]
+# snp = snp.split(':')
+# chromosome = snp[0]
+# snpLocation = int(snp[1])
+
+# read in gene annotations file
+# for line in file:
+	# line = line.rstrip('\r\n')
+	# columns = line.split('\t')
+	# geneChrom = columns[0]
+	# geneId = columns[1]
+	# geneName = columns[2]
+	# geneFeature = columns[3]
+	# geneStart = int(columns[4])
+	# geneEnd = int(columns[5])
+
+	# make dictionaries for start and end locations and the gene
+	# maybe also make a dictionary for geneId and geneName
+
+	# filter for only the chromosome specified in user argument
+	# if chromosome == geneChrom:
+		# find the start OR end position closest to the gene
+
+
+
+# read in tissue expression file (tab-separated)
