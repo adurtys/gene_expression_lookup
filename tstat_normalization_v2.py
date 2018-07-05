38fc87d04c2131328681eca5351205b17ead11a9
adurtys <adurtys@gmail.com>
2018-07-05 15:03:57 -0400

    edited v2

--- tstat_normalization_v2.py
+++ tstat_normalization_v2.py
@@ -95,7 +95,7 @@ def rankList(anyList):
 
 
 # read in the tstat file
-inFilename = "tstat_first10lines.tsv"
+inFilename = "GTEx.tstat.tsv"
 inFile = open(inFilename, 'r')
 
 # store column labels in header row
@@ -132,7 +132,6 @@ rankedMatrix = [[0 for gene in range(numGenes)] for tissue in range(numTissues)]
 # rank tissue expressions for each tissue
 for i in range(numTissues):
 	rankedMatrix[i] = rankList(matrix[i])
-	print "rankedMatrix[i]:", rankedMatrix[i]
 
 # create new file containing rank-normalized tstats
 outFilename = "normalizedGTEx.tstat.txt"
