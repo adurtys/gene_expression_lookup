a7765b2fce5999bced1cd51739429742d51d103f
adurtys <adurtys@gmail.com>
2018-07-09 15:10:20 -0400

    edited for test3

--- expression_lookup_final.py
+++ expression_lookup_final.py
@@ -190,7 +190,13 @@ distanceFromSnpDict = {}
 for gene in downstreamGenes:
 	distanceFromSnpDict[gene] = (startDict.values()).index(gene)
 for gene in upstreamGenes:
-	distanceFromSnpDict[gene] = (endDict.values()).index(gene)
+	if gene not in distanceFromSnpDict:
+		distanceFromSnpDict[gene] = (endDict.values()).index(gene)
+	else:
+		# gene is already in the dictionary
+		# modify distance in dictionary only if new distance is less than current distance
+		if (endDict.values()).index(gene) < distanceFromSnpDict[gene]:
+			distanceFromSnpDict[gene] = (endDict.values()).index(gene)
 
 closestDistances = sorted(distanceFromSnpDict.values())
 print "Closest Distances:", closestDistances
