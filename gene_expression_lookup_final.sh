2f2e4b2a4c60cba6f2012a93ff682bfd33231ff4
adurtys <adurtys@gmail.com>
2018-07-09 12:39:11 -0400

    edited shell script

--- gene_expression_lookup_final.sh
+++ gene_expression_lookup_final.sh
@@ -1,6 +1,12 @@
 # Date Created: 9 July 2018
 # Date Last Modified: 9 July 2018
-# TODO: Execution: python gene_expression_lookup_final.sh
+# Execution: python gene_expression_lookup_final.sh snpFile numGenesToSearch distanceFromSnp expressionThreshold
+# snpFile is the name of the file containing the snps for which to search
+# numGenesToSearch is the number of genes closest to the snp for which to look up tissue expression
+# distanceFromSnp is the maximum distance upstream and downstream from the snp for which to search for genes, in kbp
+# if distanceFromSnp > 1000 kbp, distanceFromSnp = 1000kbp (max distance from gene that will be searched is 1 mbp)
+# expressionThreshold is the percentage (in decimal) of top rank-ordered t-statistics that should be considered "highly
+# expressed" for each tissue
 # TODO: Description:
 
 #!/usr/bein/env bash
@@ -10,16 +16,27 @@ then
 	echo "Both the GENCODE GTF file and the t-statistics file have been processed."
 	echo "Now starting snp lookup."
 
-	# check number of command line arguments
-	if [ $# != 0 ]
+	# check number of command-line arguments
+	if [ $# = 0 ]
 	then
-		echo "something is wrong..."
+		echo "ERROR: incorrect number of command-line arguments."
+		echo "This program requires 4 command-line arguments in addition to the name of the command."
 	else
-		echo "command line contains $# arguments."
+		# store command-line arguments as variables
+		snpFile <- $1
+		numGenesToSearch <- $2
+		distanceFromSnp <- $3
+		expressionThreshold <- $4
+
+		while read -r line
+		do
+			snp <- "$line"
+			echo "snp read from file - $snp"
+			./expression_lookup_final snp numGenesToSearch distanceFromSnp expressionThreshold
+		done
+
 	fi
 	
-	# ./expression_lookup_final.py
-
 elif [ -f ./gene_annotations.txt]
 then
 	echo "GENCODE GTF file has been processed."
@@ -28,6 +45,9 @@ then
 	./tstat_normalization_final.py
 
 	echo "Now starting snp lookup."
+	# TODO: COPY FROM ABOVE ONCE COMPLETE
+	# TODO: DETERMINE RUNTIME AND CPU TIME
+
 else [ -f ./normalizedGTEx.tstat.txt]
 	echo "GTEx t-statistics file has been processed."
 	echo "Now processing GENCODE GTF file"
@@ -35,7 +55,7 @@ else [ -f ./normalizedGTEx.tstat.txt]
 	./GTF_processing_final.py
 
 	echo "Now starting snp lookup."
-
-	# ./expression_lookup_final.py
+	# TODO: COPY FROM ABOVE ONCE COMPLETE
+	# TODO: DETERMINE RUNTIME AND CPU TIME
 
 fi
