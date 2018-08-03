274af8aea0616d49433bc9c8b1f01d5d5cbc7277
adurtys <adurtys@gmail.com>
2018-08-03 14:49:09 -0400

    edited

--- centroidPositions.py
+++ centroidPositions.py
@@ -1,29 +1,44 @@
 # Date Created: 24 July 2018
 # Date Last Modified: 3 August 2018
-# Execution: python centroidPositions.py grouped_snps_filename nearestGenesFilename
+# Execution: python centroidPositions.py groupedSnpsFilename geneAnnotationsFilename
 # Description: TODO
 
 #!/usr/bin/env python
-import sys, collections
+import sys
 
 # read in command-line arguments
 groupedSnpsFilename = sys.argv[1]
-nearestGenesFilename = sys.argv[2]
+geneAnnotationsFilename = sys.argv[2]
 
-# create dictionary containing the nearest gene to each snp (key = chromosome, value = nearest gene)
-nearestGeneDict = {}
+# create dictionary containing relevant information pertaining to each gene in the gene annotations file (key = geneId; value = list of info [geneChrom, geneName, geneStart, geneEnd])
+geneAnnotations = {}
 
-# parse nearestGenesFilename
-nearestGenesFile = open(nearestGenesFilename, 'r')
-for line in nearestGenesFile:
+# populate this dictionary by parsing gene annotations file
+geneAnnotationsFile = open(geneAnnotationsFilename, 'r')
+
+for line in geneAnnotationsFile:
 	line = line.rstrip('\r\n')
+	
+	# split line on tab, return list of columns
 	columns = line.split('\t')
+	geneId = columns[0]
+	geneName = columns[1]
+	geneChrom = columns[2]
+	geneStart = int(columns[3])
+	geneEnd = int(columns[4])
 
-	snp = columns[0]
-	nearestGene = columns[1] # TODO: FIX THIS for if snp is equidistant!
+	geneInfo = []
 
-	nearestGeneDict[snp] = nearestGene
-nearestGenesFile.close()
+	# populate geneInfo with relevant info
+	geneInfo.append(geneChrom)
+	geneInfo.append(geneName)
+	geneInfo.append(geneStart)
+	geneInfo.append(geneEnd)
+
+	geneAnnotations[geneId] = geneInfo
+
+geneAnnotationsFile.close()
+print "Finished reading in gene annotations file."
 
 # create dictionary that will store groups as keys and the snps in each group as values
 snpGroupsDict = {}
@@ -37,56 +52,51 @@ for line in groupedSnpsFile:
 	groupNumber = int(columns[0])
 	snp = columns[1]
 
-	# process the snp
-	snp = snp.split(':')
-	chromosome = snp[0]
-	snpLocation = int(snp[1])
-	snpName = chromosome + ":" + str(snpLocation)
-
 	# create list of snps in each group
 	snpsInEachGroup = []
 
 	if groupNumber in snpGroupsDict: # group is already stored in the dictionary
 		# add the snp to the list of snps contained in that group
-		snpGroupsDict[groupNumber].append(snpName)
+		snpGroupsDict[groupNumber].append(snp)
 	else: # group isn't already in the dictionary
 		# add snp to the list
-		snpsInEachGroup.append(snpName)
+		snpsInEachGroup.append(snp)
 		snpGroupsDict[groupNumber] = snpsInEachGroup
-
 groupedSnpsFile.close()
+print "There are", len(snpGroupsDict), "groups on this chromosome."
+
+# create dictionary that will store the group number as the key and the centroid snp as its value
+centroidSnps = {}
 
 for group in snpGroupsDict:
-	# create dictionary containing the nearest genes for each snp in the group
-	nearestGeneToSnps = {}
+	groupSnpLocations = []
 
-	for snpInGroup in snpGroupsDict[group]:
-		if snpInGroup in nearestGeneDict:
-			nearestGeneToSnps[snpInGroup] = nearestGeneDict[snpInGroup]
-		else:
-			print "ERROR (centroidPositions.py line 67): snp doesn't have a nearest gene in the nearest genes file."
-			# TODO: handle this case!
+	for groupedSnp in snpGroupsDict[group]:
+		# process the snp
+		groupedSnp = groupedSnp.split(':')
+		chromosome = groupedSnp[0]
+		snpLocation = int(groupedSnp[1])
 
-	# the values in the nearestGeneToSnpsDict are the nearest genes to each snp in the group
-	nearestGenes = nearestGeneToSnps.values()
+		groupSnpLocations.append(snpLocation)
 
-	uniqNearestGenes = collections.Counter(nearestGenes).keys()
+	minSnpLocation = min(groupSnpLocations)
+	maxSnpLocation = max(groupSnpLocations)
+	centroidSnpLocation = (minSnpLocation + maxSnpLocation) / 2.0
 
-	if len(uniqNearestGenes) != 1:
-		# snps in the group don't all have the same gene
-		print "There are", len(uniqNearestGenes), "nearest genes for snps in group", group
+	centroidSnps[group] = centroidSnpLocation
 
+tab = "\t"
+newline = "\t"
 
+output = ""
 
+outFilename = "centroidSnps_" + groupedSnpsFilename
+outFile = open(outFilename, 'w')
 
+for group in centroidSnps:
+	output += group + tab + str(centroidSnps[group]) + newline
 
-
-
-
-
-
-
-
+outFile.write(output)
 
 
 	
