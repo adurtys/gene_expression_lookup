ac41ebde23bf81fc28d919aa826fd610a8168a68
adurtys <adurtys@gmail.com>
2018-08-20 16:29:59 -0400

    edited for four output files

--- expression_ML_table.py
+++ expression_ML_table.py
@@ -50,12 +50,6 @@ groupedSnpTypesFile.close()
 expressionVectorTableFilename = sys.argv[2]
 expressionVectorTableFile = open(expressionVectorTableFilename, 'r')
 
-outFilename = "expression_ML_table.txt"
-outFile = open(outFilename, 'w')
-
-# make dictionary storing expression vectors --> key = snp, value = expression vector
-expressionVectorDictionary = {}
-
 # store column lables in header row
 headerLine = expressionVectorTableFile.readline()
 headerLine = headerLine.rstrip('\r\n')
@@ -64,14 +58,13 @@ headers = headerLine.split('\t')
 # exclude first column when counting the number of tissues present in the file
 numTissues = len(headers) - 1
 
-# create counter variables for different types of snps
+# create data structures for the different snp types --> key = snp, value = expression vector
 numIndexSnps = 0
 numControlSnps = 0
-numSnpsNoType = 0
-numLipidTestingSnps = 0
-numLipidTrainingSnps = 0
-numT2DLikeTestingSnps = 0
-numT2DLikeTrainingSnps = 0
+lipidTestingSnps = {}
+lipidTrainingSnps = {}
+T2DLikeTestingSnps = {}
+T2DLikeTrainingSnps = {}
 
 # populate expression vector dictionary
 for line in expressionVectorTableFile:
@@ -82,57 +75,118 @@ for line in expressionVectorTableFile:
 
 	# determine snp type from dictionary --> discard snps not in snpTypeDict
 	if snp in snpTypeDict:
+		vector = []
+		for i in range(numTissues):
+			vector.append(columns[i + 1])
+
 		snpType = snpTypeDict[snp][1]
 		if snpType == "index":
 			numIndexSnps += 1
 		else: # snpType == "control"
 			numControlSnps += 1
 
-		if snpTypeDict[snp][2] == "lipid_testing":
-			numLipidTestingSnps += 1
-		elif snpTypeDict[snp][2] == "lipid_training":
-			numLipidTrainingSnps += 1
-		elif snpTypeDict[snp][2] == "T2Dlike_testing":
-			numT2DLikeTestingSnps += 1
-		else: # snpSource == "T2Dlike_training"
-			numT2DLikeTrainingSnps += 1
-		
-		vector = []
-		for i in range(numTissues):
-			vector.append(columns[i + 1])
-
-		# add snp and type (index for all?? (TODO)) to beginning of vector
+		# add snp and type to beginning of vector
 		vector.insert(0, snp)
 		vector.insert(1, snpType)
 
-		expressionVectorDictionary[snp] = vector
+		if snpTypeDict[snp][2] == "lipid_testing":
+			lipidTestingSnps[snp] = vector
+		elif snpTypeDict[snp][2] == "lipid_training":
+			lipidTrainingSnps[snp] = vector
+		elif snpTypeDict[snp][2] == "T2Dlike_testing":
+			T2DLikeTestingSnps[snp] = vector
+		else: # snpSource == "T2Dlike_training"
+			T2DLikeTrainingSnps[snp] = vector
 
-totalNumSnps = len(expressionVectorDictionary)
-if (numIndexSnps + numControlSnps != totalNumSnps):
-	print "ERROR: control + index != total"
-if (numLipidTrainingSnps + numLipidTrainingSnps + numT2DLikeTrainingSnps + numT2DLikeTestingSnps != totalNumSnps):
-	print "ERROR: lipidTesting + lipidTraining + T2Dtesting + T2Dtraining != total"
+# create output files
+lipidTestingOutFilename = "lipid_testing_expression_ML_table.txt"
+lipidTrainingOutFilename = "lipid_training_expression_ML_table.txt"
+T2DLikeTestingOutFilename = "T2D_like_testing_expression_ML_table.txt"
+T2DLikeTrainingOutFilename = "T2D_like_training_expression_ML_table.txt"
+
+# open the four output files
+lipidTestingOutFile = open(lipidTestingOutFilename, 'w')
+lipidTrainingOutFile = open(lipidTrainingOutFilename, 'w')
+T2DLikeTestingOutFile = open(T2DLikeTestingOutFilename, 'w')
+T2DLikeTrainingOutFile = open(T2DLikeTrainingOutFilename, 'w')
 
 tab = "\t"
 newline = "\n"
 
 # create new header line
 newHeaderLine = "snp" + tab + "type" + tab
+for i in range(numTissues):
+	if i < (numTissues - 1):
+		newHeaderLine += headers[i + 1] + tab
+	else: # create new line at end
+		newHeaderLine += headers[i + 1] + newline
 
-# create output file
-output = ""
+# create lipid testing output file
+lipidTestingOutput = newHeaderLine
+for snp in lipidTestingSnps:
+	snp = lipidTestingSnps[snp][0]
+	snpType = lipidTestingSnps[snp][1]
 
-for i in range(numSnps):
-	key = expressionVectorDictionary.keys()[i]
-	output += key + tab
+	lipidTestingOutput += snp + tab + snpType + tab
 
-	for j in range(numTissues):
-		if j < (numTissues - 1):
-			output += expressionVectorDictionary[key][j] + tab
+	for i in range(numTissues):
+		if i < (numTissues - 1):
+			lipidTestingOutput += lipidTestingSnps[i + 2] + tab
 		else: # create new line at the end of each vector
-			output += expressionVectorDictionary[key][j] + newline
+			lipidTestingOutput += lipidTestingSnps[i + 2] + newline
 
-outFile.write(output)
+lipidTestingOutFile.write(lipidTestingOutput)
+lipidTestingOutFile.close()
+
+# create lipid training output file
+lipidTrainingOutput = newHeaderLine
+for snp in lipidTrainingSnps:
+	snp = lipidTrainingSnps[snp][0]
+	snpType = lipidTrainingSnps[snp][1]
+
+	lipidTrainingOutput += snp + tab + snpType + tab
+
+	for i in range(numTissues):
+		if i < (numTissues - 1):
+			lipidTrainingOutput += lipidTrainingSnps[i + 2] + tab
+		else: # create new line at the end of each vector
+			lipidTrainingOutput += lipidTrainingSnps[i + 2] + newline
+
+lipidTrainingOutFile.write(lipidTrainingOutput)
+lipidTrainingOutFile.close()
+
+# create T2D-like testing output file
+T2DLikeTestingOutput = newHeaderLine
+for snp in T2DLikeTestingSnps:
+	snp = T2DLikeTestingSnps[snp][0]
+	snpType = T2DLikeTestingSnps[snp][1]
+
+	T2DLikeTestingOutput += snp + tab + snpType + tab
+
+	for i in range(numTissues):
+		if i < (numTissues - 1):
+			T2DLikeTestingOutput += T2DLikeTestingSnps[i + 2] + tab
+		else: # create new line at the end of each vector
+			T2DLikeTestingOutput += T2DLikeTestingSnps[i + 2] + newline
+
+T2DLikeTestingOutFile.write(T2DLikeTestingOutput)
+T2DLikeTestingOutFile.close()
+
+# create T2D-like training output file
+T2DLikeTrainingOutput = newHeaderLine
+for snp in T2DLikeTrainingSnps:
+	snp = T2DLikeTrainingSnps[snp][0]
+	snpType = T2DLikeTrainingSnps[snp][1]
+
+	T2DLikeTrainingOutput += snp + tab + snpType + tab
+
+	for i in range(numTissues):
+		if i < (numTissues - 1):
+			T2DLikeTrainingOutput += T2DLikeTrainingSnps[i + 2] + tab
+		else: # create new line at the end of each vector
+			T2DLikeTrainingOutput += T2DLikeTrainingSnps[i + 2] + newline
+
+T2DLikeTrainingOutFile.write(T2DLikeTrainingOutput)
+T2DLikeTrainingOutFile.close()
 
 expressionVectorTableFile.close()
-outFile.close()
